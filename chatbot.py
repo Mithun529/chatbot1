ad400ea2042f981bd6babc5e71c084fc8273ce83
Mithun529 <144579087+Mithun529@users.noreply.github.com>
2024-07-22 11:05:08 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import json
+import random
+import numpy as np
+import nltk
+import pickle
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
+# Load necessary files
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print(f"found in bag: {w}")
+    return np.array(bag)
+
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = get_response(ints, intents)
+    return res
+
+# Streamlit UI
+st.title("Chatbot")
+st.write("Welcome to the chatbot! Type a message and get a response.")
+
+user_input = st.text_input("You:", "")
+if st.button("Send"):
+    if user_input:
+        response = chatbot_response(user_input)
+        st.write(f"Chatbot: {response}")
